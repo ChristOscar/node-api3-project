0b145608627e6d27cfe7b272df43617bd0cb99e0
Christwide Oscar <triple101.games@gmail.com>
2022-02-09 17:34:33 -0600

    Initial setup and wireing the application

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const port = 6070;
+
+server.listen(port, () => {
+    console.log(`\n Server running on http://localhost:${port}\n`);
+});
